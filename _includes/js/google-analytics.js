f45aadd17c63d2d882e25f0038e1623f52defcd9
Tiago Fassoni <tiagofassoni@gmail.com>
2015-08-28 16:23:34 -0300

    Google Analytics com ID correta

--- _includes/js/google-analytics.js
+++ _includes/js/google-analytics.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-6555262-1', 'auto');
+ga('create', 'UA-38098556-2', 'auto');
 ga('send', 'pageview');
